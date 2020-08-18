8a4651e7ccf2040f3b37265270c7a85a6b596d9c
Pablo Miralles <pmirallesr@gmail.com>
2020-08-18 11:36:19 +0200

    Fix

--- dassl/engine/da/dael_gated.py
+++ dassl/engine/da/dael_gated.py
@@ -30,7 +30,7 @@ class Gate(nn.Module):
     def __init__(self, fdim, n_expert):
         super().__init__()
         self.G = nn.Linear(fdim, n_expert)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
     def forward(self, x):
         return self.softmax(self.G(x))
 
@@ -94,7 +94,7 @@ class DAELGated(TrainerXU):
         self.register_model('E', self.E, self.optim_E, self.sched_E)
         
         print('Building G')
-        self.G = Gate(fdim, self.num_classes)
+        self.G = Gate(fdim, self.dm.num_source_domains)
         self.G.to(self.device)
         print('# params: {:,}'.format(count_num_param(self.G)))
         self.optim_G = build_optimizer(self.G, cfg.OPTIM)
@@ -102,6 +102,7 @@ class DAELGated(TrainerXU):
         self.register_model('G', self.G, self.optim_G, self.sched_G)
         
     def forward_backward(self, batch_x, batch_u):
+        # Load data
         parsed_data = self.parse_batch_train(batch_x, batch_u)
         input_x, input_x2, label_x, domain_x, input_u, input_u2 = parsed_data
         input_x = torch.split(input_x, self.split_batch, 0)
@@ -109,10 +110,13 @@ class DAELGated(TrainerXU):
         label_x = torch.split(label_x, self.split_batch, 0)
         domain_x = torch.split(domain_x, self.split_batch, 0)
         domain_x = [d[0].item() for d in domain_x]
+        
         # x = data with small augmentations. x2 = data with large augmentations
         # They both correspond to the same datapoints. Same scheme for u and u2.
+        
         # Generate pseudo label
         with torch.no_grad():
+            # Unsupervised predictions
             feat_u = self.F(input_u)
             pred_u = []
             for k in range(self.dm.num_source_domains):
@@ -120,14 +124,17 @@ class DAELGated(TrainerXU):
                 pred_uk = pred_uk.unsqueeze(1)
                 pred_u.append(pred_uk)
             pred_u = torch.cat(pred_u, 1) # (B, K, C)
+            # Pseudolabel = weighted predictions
             u_filter = self.G(feat_u)
-            u_filter = u_filter.unsqueeze(1).expand(*pred_u.shape)
+            u_filter = u_filter.unsqueeze(2).expand(*pred_u.shape)
             pred_fu = (pred_u*u_filter).sum(1)
+        # Init losses
         loss_x = 0
         loss_cr = 0
         acc_x = 0
         acc_filter = 0
-
+        
+        # Supervised and unsupervised features
         feat_x = [self.F(x) for x in input_x]
         feat_x2 = [self.F(x) for x in input_x2]
         feat_u2 = self.F(input_u2)
@@ -138,21 +145,20 @@ class DAELGated(TrainerXU):
             cr_s = [j for j in domain_x if j != i]
 
             # Learning expert
-            pred_xi = self.E(i, feat_xi).unsqueeze(1)
+            pred_xi = self.E(i, feat_xi)
             expert_label_xi = pred_xi.detach()
-            loss_x += ((label_xi - pred_xi)**2).sum(1).mean()
+            loss_x += (-label_xi * torch.log(pred_xi + 1e-5)).sum(1).mean()
             acc_x += compute_accuracy(pred_xi.detach(),
                                       label_xi.max(1)[1])[0].item()
 
             x_filter = self.G(feat_xi)
-            x_filter = x_filter.unsqueeze(1).expand(*pred_xi.shape)
             # Filter must be 1 for expert, 0 otherwise
-            filter_label = torch.Tensor([0 for j in range(len(domain_x))]).to(self.device)
+            filter_label = torch.Tensor([0 for _ in range(len(domain_x))]).to(self.device)
             filter_label[i] = 1
-            filter_label = filter_label.unsqueeze(1).unsqueeze(0).expand(*pred_u.shape)
+            filter_label = filter_label.unsqueeze(0).expand(*x_filter.shape)
             loss_filter = (-filter_label * torch.log(x_filter + 1e-5)).sum(1).mean()
             acc_filter += compute_accuracy(x_filter.detach(),
-                                      filter_label.item())
+                                      filter_label.max(1)[1])[0].item()
             
             
             # Consistency regularization - Mean must follow the leading expert
@@ -161,8 +167,7 @@ class DAELGated(TrainerXU):
                 pred_j = self.E(j, feat_x2i)
                 pred_j = pred_j.unsqueeze(1)
                 cr_pred.append(pred_j)
-            cr_pred = torch.cat(cr_pred, 1)
-            cr_pred = (cr_pred*x_filter).sum(1)
+            cr_pred = torch.cat(cr_pred, 1).mean(1)
             loss_cr += ((cr_pred - expert_label_xi)**2).sum(1).mean()
 
         loss_x /= self.n_domain
@@ -179,14 +184,15 @@ class DAELGated(TrainerXU):
         pred_u = torch.cat(pred_u, 1)
         pred_u = pred_u.mean(1)
         loss_u = ((pred_fu - pred_u)**2).sum(1).mean()
-
+        
         loss = 0
         loss += loss_x
         loss += loss_cr
         loss += loss_filter
         loss += loss_u * self.weight_u
         self.model_backward_and_update(loss)
-
+        
+        print(loss_x, loss_cr, loss_filter, loss_u*self.weight_u)
         loss_summary = {
             'loss_x': loss_x.item(),
             'acc_x': acc_x,
